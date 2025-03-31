4691b023be8a6dc3435947097002729d8252ea92
Boaz <webevolution13@gmail.com>
2025-03-31 12:37:01 +0200

    upDate
    front end

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,51 +1,35 @@
 <script setup lang="ts">
-  import { fetchPosts, truncateContent } from '~/utils/js/utils'
+import { fetchPosts, truncateContent } from '~/utils/js/utils'
 
-  const itemsPost = await fetchPosts()
+const itemsPost = await fetchPosts()
 </script>
 
 <template>
-  <section class="py-10 text-white bg-black">
-    <div class="container flex flex-col">
-      <h2
-        class="max-w-3xl pb-4 uppercase text-[32px] font-extrabold font-shoulders"
-      >
-        Blog
-      </h2>
-
-      <div
-        class="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3"
-      >
-        <div
-          v-for="post in itemsPost"
-          :key="post.slug"
-          class="page-information-content__blog-content__gBox__iTextBox"
-        >
-          <NuxtLink
-            class="blog-box__link-box__Nuxtlink"
-            :to="`/posts/${post.slug}`"
-          >
-            <NuxtImg
-              height="100%"
-              loading="lazy"
-              class="page-information-content__blog-content__gBox__iTextBox__img"
-              :src="`${$config.public.apiBaseUrl}/storage/${post.image}`"
-              alt="{{ post.title }}"
-            />
-            <div
-              class="page-information-content__blog-content__gBox__iTextBox__tBox"
-            >
-              <h4
-                class="page-information-content__blog-content__gBox__iTextBox__tBox__h4 text-2xl text-white uppercase font-teko"
+  <section>
+    <div class="sales-content">
+      <div class="sales-content__elem sales-content__elem--margin-0 text-center position-relative bg-color-w">
+        <h5 class="sales-content__elem__h5 f-700">BLOG</h5>
+        <div class="sales-content__elem__linkBox bg-color-w">
+          <!-- Blog container -->
+          <div class="blog-container d-flex">
+            <div v-for="post in itemsPost" :key="post.slug" class="blog-card">
+              <NuxtLink
+                class="blog-container__Nuxtlink"
+                :to="`/posts/${post.slug}`"
               >
-                {{ post.title }}
-              </h4>
-              <p
-                class="page-information-content__blog-content__gBox__iTextBox__tBox__p"
-                v-html="truncateContent(post.body, 230)"
-              ></p>
+                <NuxtImg
+                  :src="`${$config.public.apiBaseUrl}/public/storage/${post.image}`"
+                  :alt="post.title"
+                  class="blog-card__image"
+                />
+                <h6 class="blog-card__title text-color">{{ post.title }}</h6>
+                <p
+                  class="blog-card__description text-color"
+                  v-html="truncateContent(post.body, 100)"
+                />
+              </NuxtLink>
             </div>
-          </NuxtLink>
+          </div>
         </div>
       </div>
     </div>
