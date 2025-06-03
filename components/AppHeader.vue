099280af90c450a95a7a973b69516274f54f5cd2
Boaz <webevolution13@gmail.com>
2025-06-03 21:19:47 +0200

    upDate
    front end

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -59,14 +59,14 @@ const isMenuOpen = ref(false)
     </div>
     <div class="header-content d-flex">
       <div class="logo-box header-content__logo-box">
-        <a href="/" class="header-content__logo-box__link">
+        <NuxtLink to="/" class="header-content__logo-box__link">
           <NuxtImg
             src="/img/logo.png"
             alt="Műszaki vizsgáztatás"
             class="header-content__logo-box__link__img"
             height="100%"
           />
-        </a>
+        </NuxtLink>
       </div>
       <div class="nav-box header-content__nav-box d-flex">
         <button
